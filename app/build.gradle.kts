5a3dc65e270fe0c729f4a0c6e9a73c2f1f8a71c5
User_3 <rmuhammadqodir58@gmail.com>
2023-09-06 11:19:02 +0300

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "uz.itschool.todo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "uz.itschool.todo"
@@ -33,7 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    viewBinding.enable
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -47,4 +49,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
--- app/src/main/java/uz/itschool/todo/SplashFragment.kt
+++ app/src/main/java/uz/itschool/todo/SplashFragment.kt
@@ -1,10 +1,13 @@
 package uz.itschool.todo
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import uz.itschool.todo.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
@@ -12,8 +15,12 @@ class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        val binding = FragmentSplashBinding.inflate(inflater,  container, false)
+        val handler  = Handler()
+        handler.postDelayed({
+                            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }, 700)
+        return binding.root
     }
 }
